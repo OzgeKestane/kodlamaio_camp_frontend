e26b13a7a07a273d1bb480c57745857be692969f
Özge Kestane <ozgekestane97@gmail.com>
2024-03-19 03:02:12 +0300

    add models

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ApplicationModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product',
@@ -10,11 +11,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
-  product1: any = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 }
-  product2: any = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5 }
-  product3: any = { productId: 3, productName: 'Mause', categoryId: 1, unitPrice: 5 }
-  product4: any = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5 }
-  product5: any = { productId: 5, productName: 'Table', categoryId: 1, unitPrice: 5 }
+  
 
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5];
+  products:Product[] = [];
+  constructor(){ }
+
+  ngOnInit():void { }
+
 }
