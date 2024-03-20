c253ce9d1d8776544a6771d362bdd08848ac4cb9
Özge Kestane <ozgekestane97@gmail.com>
2024-03-20 03:11:31 +0300

    product updated

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,21 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { ApplicationModule, Component } from '@angular/core';
+import { ApplicationModule, Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Product } from '../../models/product';
+import { ProductResponseModel } from '../../models/productResponseModel';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [FormsModule,ApplicationModule,CommonModule],
+  imports: [FormsModule,ApplicationModule,CommonModule,HttpClientModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
   
 
   products:Product[] = [];
-  constructor(){ }
+  //apiUrl="https://localhost:7181/api/products/getall"
+  /*productResponseModel:ProductResponseModel={
+    data:this.products,
+    message:"",
+    success:true
+  };*/
+  constructor(private httpClient:HttpClient){ } //newlemek
 
-  ngOnInit():void { }
+  ngOnInit():void { 
+    this.getProducts();
+  }
+  getProducts(){
+    this.httpClient
+    .get<ProductResponseModel>('https://localhost:7181/api/products/getall')
+    .subscribe((response)=>{
+      this.products=response.data
+    });
+
+  }
 
 }
